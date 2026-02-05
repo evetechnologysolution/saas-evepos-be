e385275888fd4afec44ef75b0cae2af513eeec3b
Aditya <aditdualapan@gmail.com>
2026-02-05 14:58:18 +0700

    update

--- models/core/service.js
+++ models/core/service.js
@@ -2,43 +2,50 @@ import mongoose from "mongoose";
 import mongoosePaginate from "mongoose-paginate-v2";
 import mongooseLeanVirtuals from "mongoose-lean-virtuals";
 
-const DataSchema = mongoose.Schema({
+const DataSchema = mongoose.Schema(
+  {
     name: {
-        type: String,
-        uppercase: true,
-        trim: true,
-        required: [true, "Name wajib diisi"],
-        // "BASIC", "START UP", "ENTERPRISE"
+      type: String,
+      uppercase: true,
+      trim: true,
+      required: [true, "Name wajib diisi"],
+      // "BASIC", "START UP", "ENTERPRISE"
     },
     price: {
-        yearly: {
-            type: Number,
-            default: 0
-        },
-        monthly: {
-            type: Number,
-            default: 0
-        }
+      yearly: {
+        type: Number,
+        default: 0,
+      },
+      monthly: {
+        type: Number,
+        default: 0,
+      },
     },
     discount: {
-        yearly: {
-            type: Number,
-            default: 0
-        },
-        monthly: {
-            type: Number,
-            default: 0
-        }
+      yearly: {
+        type: Number,
+        default: 0,
+      },
+      monthly: {
+        type: Number,
+        default: 0,
+      },
     },
     description: {
-        type: String,
-        default: ""
+      type: String,
+      default: "",
     },
     listNumber: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
-}, { timestamps: true });
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true },
+);
 
 DataSchema.plugin(mongoosePaginate);
 DataSchema.plugin(mongooseLeanVirtuals);
