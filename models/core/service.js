24f034a73973d56b81d56f6be8fc083fea03a85d
evetechnologysolution <dev@evetechsolution.com>
2026-02-05 15:04:32 +0700

    update

--- models/core/service.js
+++ models/core/service.js
@@ -2,43 +2,50 @@ import mongoose from "mongoose";
 import mongoosePaginate from "mongoose-paginate-v2";
 import mongooseLeanVirtuals from "mongoose-lean-virtuals";
 
-const DataSchema = mongoose.Schema({
-    name: {
-        type: String,
-        uppercase: true,
-        trim: true,
-        required: [true, "Name wajib diisi"],
-        // "BASIC", "START UP", "ENTERPRISE"
-    },
-    price: {
-        yearly: {
-            type: Number,
-            default: 0
+const DataSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            uppercase: true,
+            trim: true,
+            required: [true, "Name wajib diisi"],
+            // "BASIC", "START UP", "ENTERPRISE"
         },
-        monthly: {
-            type: Number,
-            default: 0
-        }
-    },
-    discount: {
-        yearly: {
-            type: Number,
-            default: 0
+        price: {
+            yearly: {
+                type: Number,
+                default: 0,
+            },
+            monthly: {
+                type: Number,
+                default: 0,
+            },
         },
-        monthly: {
+        discount: {
+            yearly: {
+                type: Number,
+                default: 0,
+            },
+            monthly: {
+                type: Number,
+                default: 0,
+            },
+        },
+        description: {
+            type: String,
+            default: "",
+        },
+        listNumber: {
             type: Number,
-            default: 0
-        }
+            default: 0,
+        },
+        isActive: {
+            type: Boolean,
+            default: true,
+        },
     },
-    description: {
-        type: String,
-        default: ""
-    },
-    listNumber: {
-        type: Number,
-        default: 0
-    },
-}, { timestamps: true });
+    { timestamps: true },
+);
 
 DataSchema.plugin(mongoosePaginate);
 DataSchema.plugin(mongooseLeanVirtuals);
