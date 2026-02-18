8b603fe842cbf4f9c9ef85f6c8e4684a4b55e04a
Aditya <aditdualapan@gmail.com>
2026-02-18 12:32:36 +0700

    wip

--- routes/cron/cron.route.js
+++ routes/cron/cron.route.js
@@ -1,11 +1,11 @@
 import express from "express";
-import {
-    checkSubsStatus
-} from "../../controllers/cron/cron.controller.js";
+import { checkSubsStatus } from "../../controllers/cron/cron.controller.js";
+import { runSubscriptionReminder } from "../../controllers/cron/reminder.controller.js";
 
 const router = express.Router();
 
 // GETTING ALL THE DATA
 router.get("/check-subscription", checkSubsStatus);
+router.get("/subscription-reminder", runSubscriptionReminder);
 
 export default router;
