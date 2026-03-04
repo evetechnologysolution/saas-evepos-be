d18a1502705e234a5261e5acc14dc756bd3d452e
Aditya <aditdualapan@gmail.com>
2026-03-04 11:28:54 +0700

    wip

--- controllers/pos/progressLabel.controller.js
+++ controllers/pos/progressLabel.controller.js
@@ -4,146 +4,160 @@ import { errorResponse } from "../../utils/errorResponse.js";
 
 // GETTING ALL THE DATA
 export const getAllData = async (req, res) => {
-    try {
-        const { page, perPage, search, sort } = req.query;
-        let qMatch = {};
+  try {
+    const { page, perPage, search, sort } = req.query;
+    let qMatch = {};
 
-        if (req.userData) {
-            qMatch.tenantRef = req.userData?.tenantRef;
-        }
-
-        if (search) {
-            const fixedId = mongoose.Types.ObjectId.isValid(search)
-                ? new mongoose.Types.ObjectId(search)
-                : null;
-
-            qMatch = {
-                ...qMatch,
-                $or: [
-                    { name: { $regex: search, $options: "i" } },
-                    ...(fixedId ? [{ _id: fixedId }] : []),
-                ], // option i for case insensitivity to match upper and lower cases.
-            };
-        }
-
-        let sortObj = { createdAt: -1 }; // default
-        if (sort && sort.trim() !== "") {
-            sortObj = {};
-            sort.split(",").forEach((rule) => {
-                const [field, type] = rule.split(":");
-                sortObj[field] = type === "asc" ? 1 : -1;
-            });
-        }
-
-        const options = {
-            page: parseInt(page, 10) || 1,
-            limit: parseInt(perPage, 10) || 10,
-            sort: sortObj,
-            lean: true,
-            leanWithId: false,
-        };
-        const listofData = await Label.paginate(qMatch, options);
-        return res.json(listofData);
-    } catch (err) {
-        return errorResponse(res, {
-            statusCode: 500,
-            code: "SERVER_ERROR",
-            message: err.message || "Terjadi kesalahan pada server",
-        });
+    if (req.userData) {
+      qMatch.tenantRef = req.userData?.tenantRef;
     }
+
+    if (search) {
+      const fixedId = mongoose.Types.ObjectId.isValid(search)
+        ? new mongoose.Types.ObjectId(search)
+        : null;
+
+      qMatch = {
+        ...qMatch,
+        $or: [
+          { name: { $regex: search, $options: "i" } },
+          ...(fixedId ? [{ _id: fixedId }] : []),
+        ], // option i for case insensitivity to match upper and lower cases.
+      };
+    }
+
+    let sortObj = { createdAt: -1 }; // default
+    if (sort && sort.trim() !== "") {
+      sortObj = {};
+      sort.split(",").forEach((rule) => {
+        const [field, type] = rule.split(":");
+        sortObj[field] = type === "asc" ? 1 : -1;
+      });
+    }
+
+    const options = {
+      page: parseInt(page, 10) || 1,
+      limit: parseInt(perPage, 10) || 10,
+      sort: sortObj,
+      lean: true,
+      leanWithId: false,
+    };
+    const listofData = await Label.paginate(qMatch, options);
+    return res.json(listofData);
+  } catch (err) {
+    return errorResponse(res, {
+      statusCode: 500,
+      code: "SERVER_ERROR",
+      message: err.message || "Terjadi kesalahan pada server",
+    });
+  }
 };
 
 export const getAllRawData = async (req, res) => {
-    try {
-        let qMatch = {};
+  try {
+    let qMatch = {};
 
-        if (req.userData) {
-            qMatch.tenantRef = req.userData?.tenantRef;
-        }
-
-        const listofData = await Label.find(qMatch).lean();
-        return res.json(listofData);
-    } catch (err) {
-        return errorResponse(res, {
-            statusCode: 500,
-            code: "SERVER_ERROR",
-            message: err.message || "Terjadi kesalahan pada server",
-        });
+    if (req.userData) {
+      qMatch.tenantRef = req.userData?.tenantRef;
     }
+
+    const listofData = await Label.find(qMatch).lean();
+    return res.json(listofData);
+  } catch (err) {
+    return errorResponse(res, {
+      statusCode: 500,
+      code: "SERVER_ERROR",
+      message: err.message || "Terjadi kesalahan pada server",
+    });
+  }
 };
 
 export const getDataById = async (req, res) => {
-    try {
-        let qMatch = { _id: req.params.id };
-        if (req.userData) {
-            qMatch.tenantRef = req.userData?.tenantRef;
-        }
-        const spesificData = await Label.findOne(qMatch).lean();
-        return res.json(spesificData);
-    } catch (err) {
-        return errorResponse(res, {
-            statusCode: 500,
-            code: "SERVER_ERROR",
-            message: err.message || "Terjadi kesalahan pada server",
-        });
+  try {
+    let qMatch = { _id: req.params.id };
+    if (req.userData) {
+      qMatch.tenantRef = req.userData?.tenantRef;
     }
+    const spesificData = await Label.findOne(qMatch).lean();
+    return res.json(spesificData);
+  } catch (err) {
+    return errorResponse(res, {
+      statusCode: 500,
+      code: "SERVER_ERROR",
+      message: err.message || "Terjadi kesalahan pada server",
+    });
+  }
 };
 
 // CREATE NEW DATA
 export const addData = async (req, res) => {
-    try {
-        let objData = req.body;
-        if (req.userData) {
-            objData.tenantRef = req.userData?.tenantRef;
-        }
-
-        const data = new Label(objData);
-        const newData = await data.save();
-        return res.json(newData);
-    } catch (err) {
-        return errorResponse(res, {
-            statusCode: 500,
-            code: "SERVER_ERROR",
-            message: err.message || "Terjadi kesalahan pada server",
-        });
+  try {
+    let objData = req.body;
+    if (req.userData) {
+      objData.tenantRef = req.userData?.tenantRef;
     }
+
+    objData.previousName = req.body.name;
+    const data = new Label(objData);
+    const newData = await data.save();
+    return res.json(newData);
+  } catch (err) {
+    return errorResponse(res, {
+      statusCode: 500,
+      code: "SERVER_ERROR",
+      message: err.message || "Terjadi kesalahan pada server",
+    });
+  }
 };
 
 // UPDATE A SPECIFIC DATA
 export const editData = async (req, res) => {
-    try {
-        let qMatch = { _id: req.params.id };
-        if (req.userData) {
-            qMatch.tenantRef = req.userData?.tenantRef;
-        }
-        let objData = req.body;
-        const updatedData = await Label.updateOne(qMatch, {
-            $set: objData,
-        });
-        return res.json(updatedData);
-    } catch (err) {
-        return errorResponse(res, {
-            statusCode: 500,
-            code: "SERVER_ERROR",
-            message: err.message || "Terjadi kesalahan pada server",
-        });
+  try {
+    let qMatch = { _id: req.params.id };
+    if (req.userData) {
+      qMatch.tenantRef = req.userData?.tenantRef;
     }
+    let objData = req.body;
+
+    const oldData = await Label.findOne(qMatch);
+
+    if (!oldData) {
+      return errorResponse(res, {
+        statusCode: 404,
+        code: "NOT_FOUND",
+        message: "Data tidak ditemukan",
+      });
+    }
+
+    objData.previousName = oldData.name;
+
+    const updatedData = await Label.updateOne(qMatch, {
+      $set: objData,
+    });
+    return res.json(updatedData);
+  } catch (err) {
+    return errorResponse(res, {
+      statusCode: 500,
+      code: "SERVER_ERROR",
+      message: err.message || "Terjadi kesalahan pada server",
+    });
+  }
 };
 
 // DELETE A SPECIFIC DATA
 export const deleteData = async (req, res) => {
-    try {
-        let qMatch = { _id: req.params.id };
-        if (req.userData) {
-            qMatch.tenantRef = req.userData?.tenantRef;
-        }
-        const deletedData = await Label.deleteOne(qMatch);
-        return res.json(deletedData);
-    } catch (err) {
-        return errorResponse(res, {
-            statusCode: 500,
-            code: "SERVER_ERROR",
-            message: err.message || "Terjadi kesalahan pada server",
-        });
+  try {
+    let qMatch = { _id: req.params.id };
+    if (req.userData) {
+      qMatch.tenantRef = req.userData?.tenantRef;
     }
+    const deletedData = await Label.deleteOne(qMatch);
+    return res.json(deletedData);
+  } catch (err) {
+    return errorResponse(res, {
+      statusCode: 500,
+      code: "SERVER_ERROR",
+      message: err.message || "Terjadi kesalahan pada server",
+    });
+  }
 };
